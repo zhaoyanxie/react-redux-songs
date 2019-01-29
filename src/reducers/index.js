b6c0b196f087b56834efccf46d0442623b5548a1
xie-zhaoyan <xiezhaoyan@amaris.ai>
2019-01-29 16:12:32 +0800

    Setup redux store

---
+++ src/reducers/index.js
@@ -0,0 +1,22 @@
+import { combineReducers } from "redux";
+
+const songsReducer = () => {
+  return [
+    { title: "song 1", duration: "1:05" },
+    { title: "song 2", duration: "2:05" },
+    { title: "song 3", duration: "3:05" },
+    { title: "song 4", duration: "4:05" }
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+  return selectedSong;
+};
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer
+});
