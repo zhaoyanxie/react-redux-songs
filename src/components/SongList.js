550ccef8fe89fd052b44f6820a2d3c2e914921fd
xie-zhaoyan <xiezhaoyan@amaris.ai>
2019-01-29 16:19:43 +0800

    Build a list of selectable song cards using semantic ui

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -2,8 +2,20 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 class SongList extends Component {
+  renderList() {
+    return this.props.songs.map((song, i) => (
+      <div className="item" key={i}>
+        <div className="right floated content">
+          <button className="ui button primary">Select</button>
+        </div>
+        <div className="content">{song.title}</div>
+      </div>
+    ));
+  }
+
   render() {
-    return <div>SongList</div>;
+    console.log(this.props);
+    return <div className="ui divided list">{this.renderList()}</div>;
   }
 }
 
